9a392a447e8dc61ce93ca7c3d1b6d92f63cf6de5
Lenny Ruprecht <lenny@vinivia.com>
2023-10-25 09:24:36 +0200

    - Shows Homepage in App - Adds Portrait image

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Loader from "./components/Loader/Loader";
 import Home from "./pages/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [progress, setProgress] = useState(0);
 
+  const preloadImages = (imagesArray) => {
+    return Promise.all(
+      imagesArray.map((src) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = src;
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      })
+    );
+  };
+
   useEffect(() => {
+    const imagesArray = [
+      "/portfolio-page/img/Hero_Image.png",
+      "/portfolio-page/img/Bedroom_Homepage.png",
+      "/portfolio-page/img/Living_Room2.png",
+      "/portfolio-page/img/Portrait_Lenny.png",
+    ];
+    preloadImages(imagesArray).catch((error) =>
+      console.error("Image loading failed:", error)
+    );
     const loadTimer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
 
     const progressInterval = setInterval(() => {
       setProgress((prevProgress) => {
@@ -20,7 +43,7 @@ function App() {
         }
         return prevProgress + 1;
       });
-    }, 20);
+    }, 10);
 
     return () => {
       clearTimeout(loadTimer);
